983b8a014ee4e737607083dd2d2875bf7c3d971e
Valehvvv <142462847+Valehvvv@users.noreply.github.com>
2023-10-27 23:06:47 -0300

    Update Main.java

--- kh/src/main/java/org/example/Main.java
+++ kh/src/main/java/org/example/Main.java
@@ -1,152 +1,122 @@
 package org.example;
+import java.util.*;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-// Clase Producto
-public class Producto {
+class Producto {
     private String nombre;
     private int stock;
     private int codigoBarra;
     private int precio;
 
-    public Producto(String nombre, int stock, int codigoBarra, int precio) {
-        this.nombre = nombre;
-        this.stock = stock;
-        this.codigoBarra = codigoBarra;
-        this.precio = precio;
-    }
-
-    // Métodos getter y setter para acceder y modificar los atributos
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
+
     public int getStock() { return stock; }
     public void setStock(int stock) { this.stock = stock; }
+
     public int getCodigoBarra() { return codigoBarra; }
     public void setCodigoBarra(int codigoBarra) { this.codigoBarra = codigoBarra; }
+
     public int getPrecio() { return precio; }
     public void setPrecio(int precio) { this.precio = precio; }
 }
 
-// Clase Usuario
-public class Usuario {
+class Usuario {
     private String nombreUsuario;
     private String nombreEmpresa;
     private String contraseña;
 
-    public Usuario(String nombreUsuario, String nombreEmpresa, String contraseña) {
-        this.nombreUsuario = nombreUsuario;
-        this.nombreEmpresa = nombreEmpresa;
-        this.contraseña = contraseña;
-    }
-
-    // Métodos getter y setter para acceder y modificar los atributos
     public String getNombreUsuario() { return nombreUsuario; }
     public void setNombreUsuario(String nombreUsuario) { this.nombreUsuario = nombreUsuario; }
+
     public String getNombreEmpresa() { return nombreEmpresa; }
     public void setNombreEmpresa(String nombreEmpresa) { this.nombreEmpresa = nombreEmpresa; }
+
     public String getContraseña() { return contraseña; }
     public void setContraseña(String contraseña) { this.contraseña = contraseña; }
 }
 
-// Clase GestorUsuario
-public class GestorUsuario {
-    private List<Usuario> listaUsuarios;
+class GestorDeUsuario {
+    private List<Usuario> listaUsuarios = new ArrayList<>();
 
-    public GestorUsuario() {
-        this.listaUsuarios = new ArrayList<>();
-    }
+    public void agregar(Usuario usuario) { listaUsuarios.add(usuario); }
+    
+    public void eliminar(Usuario usuario) { listaUsuarios.remove(usuario); }
 
-    public void crearUsuario(String nombreUsuario, String nombreEmpresa, String contraseña) {
-        Usuario nuevoUsuario = new Usuario(nombreUsuario, nombreEmpresa, contraseña);
-        listaUsuarios.add(nuevoUsuario);
-    }
+}
 
-    public boolean iniciarSesion(String nombreUsuario, String contraseña) {
-        for (Usuario usuario : listaUsuarios) {
-            if (usuario.getNombreUsuario().equals(nombreUsuario) && usuario.getContraseña().equals(contraseña)) {
-                return true;
+class Inventario {
+    private List<Producto> listaProductos = new ArrayList<>();
+
+    public Producto buscarProductoPorNombre(String nombre) {
+        for (Producto producto : listaProductos) {
+            if (producto.getNombre().equals(nombre)) {
+                return producto;
             }
         }
-        return false;
+        return null;
     }
 
-    public List<Usuario> getListaUsuarios() { return listaUsuarios; }
-    public void setListaUsuarios(List<Usuario> listaUsuarios) { this.listaUsuarios = listaUsuarios; }
+    public Producto buscarProductoPorCodigo(int codigo) {
+        for (Producto producto : listaProductos) {
+            if (producto.getCodigoBarra() == codigo) {
+                return producto;
+            }
+        }
+        return null;
+    }
+
 }
 
-// Clase Menu
-public class Menu {
-    private GestorUsuario gestorUsuario;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        GestorDeUsuario gestorDeUsuario = new GestorDeUsuario();
+        Inventario inventario = new Inventario();
+        Usuario usuarioActual = null;
 
-    public Menu() {
-        this.gestorUsuario = new GestorUsuario();
-    }
+        while (true) {
+            System.out.println("1. Iniciar sesión");
+            System.out.println("2. Crear usuario");
+            System.out.println("3. Salir");
+            int opcion = scanner.nextInt();
 
-    // Método para mostrar el menú
-    // Nota: Este método requiere interacción del usuario, por lo que no funcionará en este entorno.
-}
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.exit(0);
+            }
 
-// Clase Registro
-public class Registro {
-    private String fecha;
-    private Producto nombreProducto;
+            if (usuarioActual != null) {
+                while (true) {
+                    System.out.println("1. Agregar producto al inventario");
+                    System.out.println("2. Eliminar producto del inventario");
+                    System.out.println("3. Buscar producto por nombre");
+                    System.out.println("4. Buscar producto por código");
+                    System.out.println("5. Cerrar sesión");
+                    int subOpcion = scanner.nextInt();
 
-    public Registro(String fecha, Producto nombreProducto) {
-        this.fecha = fecha;
-        this.nombreProducto = nombreProducto;
-    }
+                    switch (subOpcion) {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            usuarioActual = null;
+                            break;
+                    }
 
-    // Método para agregar a un CSV
-    public void agregarRegistroCSV(String archivo) {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(archivo, true))) {
-            writer.write(fecha + "," + nombreProducto.getNombre() + "," + nombreProducto.getCodigoBarra());
-            writer.newLine();
-        } catch (IOException e) {
-            e.printStackTrace();
+                    if (usuarioActual == null) {
+                        break;
+                    }
+                }
+            }
         }
     }
-}
-
-// Clase Stock
-public class Stock {
-    private String fechaStock;
-    private Producto productoModificado;
-    private int cantidadModificar;
-
-    public Stock(String fechaStock, Producto productoModificado, int cantidadModificar) {
-        this.fechaStock = fechaStock;
-        this.productoModificado = productoModificado;
-        this.cantidadModificar = cantidadModificar;
-    }
-
-    public String obtenerFechaModificacion() { return fechaStock; }
-    public String getFechaStock() { return fechaStock; }
-    public void setFechaStock(String fechaStock) { this.fechaStock = fechaStock; }
-    public Producto getProductoModificado() { return productoModificado; }
-    public void setProductoModificado(Producto productoModificado) { this.productoModificado = productoModificado; }
-    public int getCantidadModificar() { return cantidadModificar; }
-    public void setCantidadModificar(int cantidadModificar) { this.cantidadModificar = cantidadModificar; }
-}
-
-// Clase HistorialStock
-public class HistorialStock {
-    private List<Stock> entrada;
-    private List<Stock> salida;
-    private List<Stock> cambiosStock;
-
-    public HistorialStock() {
-        this.entrada = new ArrayList<>();
-        this.salida = new ArrayList<>();
-        this.cambiosStock = new ArrayList<>();
-    }
-
-    public void registrarEntrada(Stock entrada) { this.entrada.add(entrada); }
-    public void registrarSalida(Stock salida) { this.salida.add(salida); }
-    public List<Stock> obtenerEntrada() { return entrada; }
-    public List<Stock> obtenerSalida() { return salida; }
-    public List<Stock> obtenerCambioStock() { return cambiosStock; }
 }
